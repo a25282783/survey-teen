eac33c7565adf5642b57b767e72dba4c74fd0c7c
lliu <lliu@elta.com.tw>
2022-09-25 13:25:27 +0800

    page2 controller js架構

--- app/Http/Controllers/Page3Controller.php
+++ app/Http/Controllers/Page3Controller.php
@@ -12,39 +12,103 @@ class Page3Controller extends Controller
         $this->middleware('auth');
     }
 
+    private function answerArray($key, $num)
+    {
+        $res = [];
+        $input = request()->input($key);
+        if ($input && is_array($input)) {
+            for ($i = 1; $i < $num + 1; $i++) {
+                $res[$key . '_' . $i] = '0';
+                if (in_array($i, $input)) {
+                    $res[$key . '_' . $i] = '1';
+                }
+            }
+        } else {
+            for ($i = 1; $i < $num + 1; $i++) {
+                $res[$key . '_' . $i] = '0';
+            }
+        }
+        return $res;
+    }
+
     public function page3(Request $request)
     {
         $input = $request->except(['_token']);
         $data = [
-            "B-1" => [
-                'answer' => data_get($input, "B-1", ''),
+            "B_1" => [
+                'answer' => data_get($input, "B_1", '')
             ],
-            "B-1-1" => [
-                'answer' => data_get($input, "B-1-1", ''),
+            "B_1_1" => [
+                'answer' => $this->answerArray("B_1_1", 6)
             ],
-            "B-1-2" => [
-                'answer' => data_get($input, "B-1-2", ''),
+            "B_1_2" => [
+                'answer' => data_get($input, "B_1_2", '')
             ],
-            "B-1-2-6" => [
-                'answer' => data_get($input, "B-1-2-6", ''),
+            "B_2" => [
+                'answer' => $this->answerArray("B_2", 11)
             ],
-            "B-2" => [
-                'answer' => data_get($input, "B-2", ''),
+            "B_2_1" => [
+                'answer' => data_get($input, "B_2_1", '')
             ],
-            "B-2-1" => [
-                'answer' => data_get($input, "B-2-1", ''),
+            "B_3" => [
+                'answer' => data_get($input, "B_3", '')
             ],
-            "B-2-2" => [
-                'answer' => data_get($input, "B-2-2", ''),
+            "B_3_1" => [
+                'answer' => $this->answerArray("B_3_1", 9)
             ],
-            "B-3-1" => [
-                'answer' => data_get($input, "B-3-1", ''),
+            "B_3_2" => [
+                'answer' => data_get($input, "B_3_2", '')
             ],
-            'B-3-2' => [
-                'answer' => data_get($input, 'B-3-2', ''),
+            "B_4" => [
+                'answer' => data_get($input, "B_4", '')
             ],
-            "B-3-3" => [
-                'answer' => data_get($input, "B-3-3", ''),
+            "B_5" => [
+                'answer' => data_get($input, "B_5", '')
+            ],
+            "B_5_1" => [
+                'answer' => data_get($input, "B_5_1", '')
+            ],
+            "B_5_2" => [
+                'answer' => $this->answerArray("B_5_2", 8)
+            ],
+            "B_5_3" => [
+                'answer' => data_get($input, "B_5_3", '')
+            ],
+            "B_6" => [
+                'answer' => $this->answerArray("B_6", 7)
+            ],
+            "B_6_1" => [
+                'answer' => data_get($input, "B_6_1", '')
+            ],
+            "B_7" => [
+                'answer' => data_get($input, "B_7", '')
+            ],
+            "B_7_1" => [
+                'answer' => data_get($input, "B_7_1", '')
+            ],
+            "B_7_2" => [
+                'answer' => data_get($input, "B_7_2", '')
+            ],
+            "B_8" => [
+                'answer' => data_get($input, "B_8", '')
+            ],
+            "B_9" => [
+                'answer' => data_get($input, "B_9", '')
+            ],
+            "B_10" => [
+                'answer' => data_get($input, "B_10", '')
+            ],
+            "B_10_1" => [
+                'answer' => data_get($input, "B_10_1", '')
+            ],
+            "B_11" => [
+                'answer' => data_get($input, "B_11", '')
+            ],
+            "B_11_1" => [
+                'answer' => data_get($input, "B_11_1", '')
+            ],
+            "B_11_2" => [
+                'answer' => data_get($input, "B_11_2", '')
             ],
         ];
         Result::updateOrCreate(
--- public/js/page2.js
+++ public/js/page2.js
@@ -1,47 +1,64 @@
 $(function () {
-    $('input[name="B-1"]').on('change',()=>{
-        cancel('B-1')
-    })
-    $('input[name="B-2"]').on('change',()=>{
-        cancel('B-2')
-    })
-    $('input[name="B-2-1"]').on('change',()=>{
-        $('input[type="radio"][name="B-2-2"]').prop("checked",false)
-    })
+    // $('input[name="B-1"]').on('change',()=>{
+    //     cancel('B-1')
+    // })
 })
-function checkForm() {
-    // if($('#B-1-1').is(':checked')){
-    //     if(!$('select[name="B-1-1"]').val()){
-    //       $('#2-1-1err').toggleClass("d-none");
-    //       var target_top = $('#2-1-1err').offset().top-20;
-    //       $("html,body").scrollTop(target_top);
-    //       return false;
-    //     }
-    // }
-    // if($('#B-1-2').is(':checked')){
-    //     if(!$('input[name="B-1-2"]:checked').val()){
-    //       $('#2-1-2err').toggleClass("d-none");
-    //       var target_top = $('#2-1-2err').offset().top-20;
-    //       $("html,body").scrollTop(target_top);
-    //       return false;
-    //     }
-    // }
-    // if($('#B-2-1').is(':checked')){
-    //   if(!$('input[name="B-2-1"]:checked').val()){
-    //       $('#2-2-1err').toggleClass("d-none");
-    //       var target_top = $('#2-2-1err').offset().top-20;
-    //       $("html,body").scrollTop(target_top);
-    //       return false;
-    //     }
-    //   if(!$('input[name="2-2-1err2"]').val()){
-    //     $('#2-2-1err2').toggleClass("d-none");
-    //     var target_top = $('#2-2-1err2').offset().top-20;
-    //     $("html,body").scrollTop(target_top);
-    //     return false;
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    // A_1:'',
+    watch_flag:true
+  },
+  methods:{
+    updateData(){
+        for (const key in this.$data) {
+            if (key === 'watch_flag') {
+                continue
+            }
+            var element = this.$refs[key];
+            if (element == undefined) {
+              continue
+            }
+            var tagName = element.tagName.toLowerCase()
+            if (tagName == 'select') {
+                this[key] =$('select[name="'+element.name+'"]').val()
+            }
+            if (tagName == 'input') {
+                var type = element.type.toLowerCase()
+                if (type == 'text' || type == 'number') {
+                    this[key] =$('input[name="'+element.name+'"]').val()
+                } else if (type == 'radio') {
+                    this[key] = $('input[name="'+this.$refs[key].name+'"]:checked').val()
+                } else if (type == 'checkbox') {
+                    this[key] = $('input[name="'+this.$refs[key].name+'"]:checked').map(function(){
+                        return $(this).val();
+                    }).get()
+                }
+
+            }
+        }
+    }
+},
+  mounted(){
+    this.watch_flag = false
+    renderAnswer();
+    this.updateData();
+  },
+  watch:{
+    // A_4(value) {
+    //   if (value == 1) {
+    //       $('input[name="A_4_1[]"]').prop('checked',false)
+    //       this.A_4_1=[];
+    //       this.A_4_2='';
+    //       this.A_4_3='';
     //   }
-    // }
-    // alert('ok')
-    // return false;
+    // },
+
+  }
+})
+
+function checkForm() {
     // final
     if ($('#form').find('input[name=save-return]').length > 0) {
         if (confirm('儲存本頁')){
@@ -54,5 +71,9 @@ function checkForm() {
         return confirm('確認前往下一頁嗎？');
       }
 }
+
+setTimeout(function(){
+  app.watch_flag = true
+}, 1500)
 
 
