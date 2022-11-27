e546967dbae718ff615d9ea4cb954b482cc9781e
lliu <lliu@elta.com.tw>
2022-11-27 16:08:54 +0800

    20221127

--- app/Exports/ResultsExport.php
+++ app/Exports/ResultsExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Result;
+use Illuminate\Support\Arr;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
@@ -15,12 +16,14 @@ class ResultsExport implements FromCollection, WithHeadings
     {
         $this->start = $start;
         $this->end = $end;
+        $this->result = $this->getFormatResult();
     }
 
     public function headings(): array
     {
-        $first = Result::first()->result;
-        return array_keys($first);
+        // $first = Result::first()->result;
+        // return array_keys($first);
+        return $this->result[0]->keys()->all();
     }
 
     /**
@@ -28,19 +31,68 @@ class ResultsExport implements FromCollection, WithHeadings
      */
     public function collection()
     {
-        $resultModel = Result::select('result');
+
+        $data = $this->result;
+        return collect($data);
+
+    }
+
+    public function getFormatResult()
+    {
+        $resultModel = Result::select('page1','page2','page3','page4','page5');
         if (!empty($this->start)) {
             $resultModel = $resultModel->whereDate('created_at', '>=', $this->start);
         }
         if (!empty($this->end)) {
             $resultModel = $resultModel->whereDate('created_at', '<=', $this->end);
         }
-        $resultModel = $resultModel->get();
-        $data = [];
-        foreach ($resultModel as $v) {
-            $data[] = collect($v->result);
-        }
-        return collect($data);
+        $resultModel = $resultModel->get()->toArray();
 
+        $data = [];
+        foreach ($resultModel as $result) {
+            // page1
+            $result['page1']['A_2_1_4other']['answer'] = $result['page1']['A_2_1_3']['answer'];
+            unset($result['page1']['A_2_1_3']);
+            // page1
+            $result['page1']['A_2_2_3other']['answer'] = $result['page1']['A_2_2_1']['answer'];
+            unset($result['page1']['A_2_2_1']);
+            // page1
+            $result['page1']['A_2_3_8other']['answer'] = $result['page1']['A_2_3_1']['answer'];
+            unset($result['page1']['A_2_3_1']);
+            // page2
+            $result['page2']['B_2_11other']['answer'] = $result['page2']['B_2_1']['answer'];
+            unset($result['page2']['B_2_1']);
+            // page2
+            $result['page2']['B_6_6other']['answer'] = $result['page2']['B_6_1']['answer'];
+            unset($result['page2']['B_6_1']);
+            // page4
+            $result['page4']['D_5_12other']['answer'] = $result['page4']['D_5_1']['answer'];
+            unset($result['page4']['D_5_1']);
+
+            $newArray = Arr::collapse([
+                $result['page1'],
+                $result['page2'],
+                $result['page3'],
+                $result['page4'],
+                $result['page5']
+            ]);
+            // 整理資料 $newRes返回一維數組
+            $newRes = [];
+            foreach ($newArray as $k => $v) {
+                if (is_string($v)) {
+                    $newRes[$k] = $v;
+                } else {
+                    if (is_array($v['answer'])) {
+                        foreach ($v['answer'] as $k1 => $v1) {
+                            $newRes[$k1] = $v1;
+                        }
+                    } else {
+                        $newRes[$k] = $v['answer'];
+                    }
+                }
+            }
+            $data[] = collect($newRes)->sortKeys();
+        }
+        return $data;
     }
 }
